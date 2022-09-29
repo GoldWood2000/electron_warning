19e6fbe68d5a58e73ae47f23a29ac1e009228174
leo <2058386844@qq.com>
2022-09-29 15:05:30 +0800

    update

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 export default {
-  host: 'http://10.10.10.120:30205/',
-  // host: 'http://10.3.2.3:30205/',
-  loginUser: {
+  host: 'http://10.10.10.120:30205/',     //公司本地
+  // host: 'http://10.3.2.3:30205/',      //长春现场
+  loginUser: {                            //公司本地
     "username": "ddadmin",
     "password": "Gdjt@2021"
   },
-  interval: 5000
+  interval: 30000
 }
